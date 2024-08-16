435233c1bb2bc6518ec055f810820ad031c89d53
Jack <sjadoenath69@gmail.com>
2024-08-17 00:20:59 +0200

    # [CHANGED] Character Model Update + Sword Update + Rotate character towards movement direction

--- player/player.gd
+++ player/player.gd
@@ -6,4 +6,6 @@ func _physics_process(delta: float) -> void:
 	var input_dir: Vector2 = Input.get_vector("left", "right", "up", "down")
 	var direction: Vector3 = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, PI/4).normalized()
 	velocity = direction * SPEED
+	if direction:
+		$Guy.rotation.y = atan2(velocity.x,velocity.z)
 	move_and_slide()
