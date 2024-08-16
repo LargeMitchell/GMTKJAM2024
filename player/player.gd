e3a57d600b7a9a373d224e8ccbf285382aa34db1
LargeMitchell <ozansomak@gmail.com>
2024-08-16 21:56:23 +0200

    Added gym and removed unnecessary code

--- player/player.gd
+++ player/player.gd
@@ -3,17 +3,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, PI/4).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-	print(Vector2(velocity.x, velocity.z))
-
+	var input_dir: Vector2 = Input.get_vector("left", "right", "up", "down")
+	var direction: Vector3 = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, PI/4).normalized()
+	velocity = direction * SPEED
 	move_and_slide()
