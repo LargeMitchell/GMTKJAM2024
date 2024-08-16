cdae109219c0ba0b6af7635545f3a7fd80dfc02d
LargeMitchell <ozansomak@gmail.com>
2024-08-16 21:28:12 +0200

    WASD movement added

--- player/player.gd
+++ player/player.gd
@@ -5,7 +5,7 @@ const SPEED = 5.0
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, PI/4).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
