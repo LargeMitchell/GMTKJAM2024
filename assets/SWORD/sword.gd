1bc1bfa788e4c2782515b8229ef42262e0802c39
Jack <sjadoenath69@gmail.com>
2024-08-17 18:39:56 +0200

    [CHANGED] Sword stays levelled above ground after levelling up

--- assets/SWORD/sword.gd
+++ assets/SWORD/sword.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 var Damage: int = 10
 var TempVec: Vector3 = Vector3(2.0,2.0,2.0)
-
+var LeveledRotation = Vector3(-44.8,9.3,132.6)
 @export var exp_component: Node
 
 func _ready() -> void:
@@ -11,4 +11,6 @@ func _ready() -> void:
 func _on_exp_component_level_up_signal() -> void:
 	print("Sword Grows")
 	self.scale_object_local(TempVec)
+	self.rotate_object_local(LeveledRotation, 0.0)
+	
 	pass # Replace with function body.
