6bd71cf2f78ada469d8b6aaee42937bf043ebd91
Jack <sjadoenath69@gmail.com>
2024-08-17 17:04:00 +0200

    [CHANGED] Signal for leveling up in Sword

--- assets/SWORD/sword.gd
+++ assets/SWORD/sword.gd
@@ -1,2 +1,9 @@
 extends Node3D
 var Damage: int = 10
+@export var exp_component: Node
+
+
+
+func _on_exp_component_level_up_signal() -> void:
+	print("Sword Grows")
+	pass # Replace with function body.
