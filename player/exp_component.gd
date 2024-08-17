5199c4ad723851273dd4bf67ec7addbaad0ff902
LargeMitchell <ozansomak@gmail.com>
2024-08-17 13:19:20 +0200

    Added leveling system to exp

--- player/exp_component.gd
+++ player/exp_component.gd
@@ -1,11 +1,22 @@
 extends Node
 
 var current_exp: int = 0
+var current_level: int = 1
+
+var level_exp_dictionary: Dictionary = {
+	1: 30,
+	2: 80,
+	3: 200,
+}
 
 func _ready() -> void:
 	Global.got_exp.connect(got_exp)
-	print(current_exp)
 
 func got_exp(amount: int) -> void:
 	current_exp += amount
-	print(current_exp)
+	if current_exp >= level_exp_dictionary[current_level]:
+		level_up()
+
+func level_up() -> void:
+	current_level += 1
+	print("level up", str(current_level))
