86162a8c7a47240cf03779a6272462b14eb9c06c
Jénilson Martins Da Silva <jenilsoncaboman@hotmail.com>
2024-08-17 11:30:56 +0200

    Added Area3D to Sword
    Calls Parent_enemy.hit(), but unsure what body is needed. @Miepasie

---
+++ assets/SWORD/sword.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var SwordDamage:int = 10
+@onready var Swordarea_3d: Area3D = $Area3D
+@onready var test_enemy: Enemy = $"test_enemy"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	test_enemy.hit(SwordDamage, test_enemy.body)
