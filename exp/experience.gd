3a2066f3cbe6d58fb7061e0d033f2deb9982d351
LargeMitchell <ozansomak@gmail.com>
2024-08-17 14:23:42 +0200

    Updated the movement for exp particle to smoothen into player

--- exp/experience.gd
+++ exp/experience.gd
@@ -1,13 +1,20 @@
 extends MeshInstance3D
 
-@export var move_speed: float = 10.0
+@export var move_speed: float = 2.0
+@export var max_speed: float = 10.0
 @export var exp_amount: int = 10
 
+var timer: float = 0.0
+
 func _physics_process(delta: float) -> void:
+	if timer < 1.0: timer += delta
+	
+	
+	var speed: float = lerpf(move_speed, max_speed, timer)
 	var pos: Vector3 = Vector3(Global.player.global_position.x, 1.0, Global.player.global_position.z)
 	var dir: Vector3 = (pos - global_position)
-	global_position += dir.normalized() * move_speed * delta
-	if dir.length() < 0.5:
+	global_position += dir.normalized() * speed * delta
+	if dir.length_squared() < pow(0.2, 2):
 		Global.got_exp.emit(exp_amount)
 		queue_free()
 	
