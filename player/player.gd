61affc364b82df82273ba25e3e89c64c05ea3b82
Jack <sjadoenath69@gmail.com>
2024-08-17 12:25:28 +0200

    [CHANGED] Walk animation active when player moves, new attack animation

--- player/player.gd
+++ player/player.gd
@@ -8,7 +8,7 @@ const SPEED = 5.0
 func _ready() -> void:
 	Global.player = self
 	
-	animation_player.play("AttackPose")
+	animation_player.play("Idle")
 
 func _physics_process(delta: float) -> void:
 	var input_dir: Vector2 = Input.get_vector("left", "right", "up", "down")
@@ -16,4 +16,5 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * SPEED
 	if direction:
 		mesh_root.global_rotation.y = atan2(velocity.x,velocity.z)
+		animation_player.play("Walk")
 	move_and_slide()
