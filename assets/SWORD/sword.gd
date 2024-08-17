6553daaeec71f05a62a12c1bbd37edc477d0f51f
Miepasies <tim.rasenberg0@gmail.com>
2024-08-17 13:12:42 +0200

    Closer to collision solution (ish) for sword/enemies

--- assets/SWORD/sword.gd
+++ assets/SWORD/sword.gd
@@ -1,22 +1,2 @@
 extends Node3D
-
-var SwordDamage:int = 10
-@onready var Swordarea_3d: Area3D = $Area3D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _on_area_3d_area_entered(area: Area3D) -> void:
-	pass
-
-
-func _on_area_3d_body_entered(EnemyBody: Node3D) -> void:
-	print("Im hitting I swear")
-	if EnemyBody.is_in_group("Enemy"):
-		EnemyBody.hit(SwordDamage)
+var Damage: int = 10
