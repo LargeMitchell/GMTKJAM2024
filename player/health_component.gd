e3d91aa95a8cae00b748aac37510606f52afd53e
Jack <sjadoenath69@gmail.com>
2024-08-19 16:17:44 +0200

    Comments to HealthComponent.gd

--- player/health_component.gd
+++ player/health_component.gd
@@ -18,11 +18,11 @@ func refill_health() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+#Player gets damaged here
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("Enemy"):
 		reduce_health()
-		
+	#Check player death if 0 or below load death screen	
 	if Player_Stats.current_health <= 0:
 		print("death of player")
 		get_tree().change_scene_to_file("res://assets/Menu/DeathScreen.tscn")
